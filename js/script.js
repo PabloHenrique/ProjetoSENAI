b921dde5ffe64e4c13e15d6644f28f288d026ca2
PabloHenrique <pablohenriquenadai@gmail.com>
2022-01-24 21:07:56 -0300

    2022 01 24 - Start FrontEnd

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function goCalcular(){
+    window.location="../pages/calcular.php";
+}
+
+function goFormulas(){
+    window.location="../pages/formcalc.php";
+}
+
+function goAbout(){
+    window.location="../pages/about.html";
+}
